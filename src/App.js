f40b57658e15a819e36e03521efe196251c85e7b
gabriela tururu <gabriela.aguayaire@gmail.com>
2022-03-28 08:05:19 -0600

    Desafio3 Landing_Testing Code 1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 // --------------- Modificada Runat -----------------//
 const App = () => {
   
-  const styles  = {
-    color: 'aqua',
-    backgroundColor: 'black',
-    borderRadius: '25%',
-    fontSize: '55px'
-    }
-
-
-  //etiqueta header modificada - de englobar toda la app a englobar solo el navBar + estilos de class App-header "comentados" para que no apliquen por ahora. 
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
-      </header>
-        <h1 style={{color:'black'}}>Bienvenidx a RumboNaturaleza</h1>   
-        {/* <h2>Conoce Las Ofertas de la Semana...</h2>      */}
-        <h2>Creando Branch D3Landing...</h2>     
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p style={styles}>Desafío 2 - Creando La NavBar</p>        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-    
+      </header>        
+          <h1 className="itemslistcontainer" greetings="Bienvenidx a RumboNaturaleza"></h1>          
+          <h2>Desafío 3 - Creando Landing (CartWidget + ItemsListContainer)</h2> 
     </div>
-  );
-}
+  )   
+} 
 
-// ----------------Original ---------------------//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1 style={{color:'green'}}>Bienvenidx a RumboNaturaleza</h1>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Desafío 2 - Creando La NavBar</p>
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>        
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-export default App;
+export default App
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+
+const ItemListContainer = (props) => {
+
+    return (
+    <h1 className="itemslistcontainer" >{props.greetings}</h1>
+    )
+}
+
+
+export default ItemListContainer
