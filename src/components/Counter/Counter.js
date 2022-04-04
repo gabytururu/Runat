0e0cc360a8554775c8434ac816cb83bfb361ac96
gabriela tururu <gabriela.aguayaire@gmail.com>
2022-04-04 14:49:54 -0500

    clase4-ComponentsII-corregido-hooks-useEffect-change+desmontar

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -6,8 +6,26 @@ const Counter = () => {
     const [count, setCount] = useState(0)
 
     useEffect(() => {
-        console.log('se acaba de montar el componente, yo me espere por el useEffect post mount')        
+        console.log('se acaba de montar el componente, yo me espere por el useEffect post mount') 
+        setTimeout (() => {
+            setCount(10)
+        },3000)    
+        
+        return (() => {
+            console.log("este se ejecuta antes de desmontar el componente")
+        })
+        
     }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('segundo timeOut con 5 seg de delay y sin alteraciones del estado')
+        }, 5000)
+    }, [])
+
+    useEffect (() => {
+        console.log('cambió el contador')
+    }, [count])
 
     const decrement = () => {
         setCount(count - 1)
@@ -18,7 +36,7 @@ const Counter = () => {
     }
 
     console.log (count)
-    console.log ('se va a montar el componente')
+    console.log ('se va a montar el componente, esto esta en el cuerpo del componente')
 
     return(
 
