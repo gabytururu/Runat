269b6f13fa9c11db0349b1b67b0e830589512b28
gabriela tururu <gabriela.aguayaire@gmail.com>
2022-04-11 11:40:54 -0500

    Desafio6- Primer intento- correcciones realizadas, renderización corregida

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -7,19 +7,19 @@ const Details = ({prodDetails}) => {
         <section className="boxID">
             <div className="cardID">
                 <div className="detallesID">
-                    <h3>{prodDetails.name}</h3>
-                    <p>{prodDetails.category}</p>
-                    <p>{prodDetails.description}</p>
+                    <h3>{prodDetails[0].name}</h3>
+                    <p>{prodDetails[0].category}</p>
+                    <p>{prodDetails[0].description}</p>
                 </div>
                 <picture>
-                    <img src={prodDetails.img} alt={prodDetails.name} className="imgID"/>
+                    <img src={prodDetails[0].img} alt={prodDetails[0].name} className="imgID"/>
                 </picture>
                 <div className="detallesID">
-                    <p>{prodDetails.serviciosBrindados}</p>
-                    <p>{prodDetails.precio}</p>
-                    <p>{prodDetails.puntoPartida}</p>
-                    <p>{prodDetails.fechas}</p>
-                    <p>{prodDetails.reserva}</p>
+                    <p>{prodDetails[0].serviciosBrindados}</p>
+                    <p>{prodDetails[0].precio}</p>
+                    <p>{prodDetails[0].puntoPartida}</p>
+                    <p>{prodDetails[0].fechas}</p>
+                    <p>{prodDetails[0].reserva}</p>
                     <button className="botonCompraID">COMPRAR</button>
                 </div>         
             </div>
--- src/components/asyncmock/asyncmock.js
+++ src/components/asyncmock/asyncmock.js
@@ -3,9 +3,9 @@ import './asyncmock.css'
 
 // ------------ ItemLists -----------------------//
 const products = [
-    {id: 1, name: 'TourA- Nevado de Toluca', price:100, category: 'biking', stock:25, description: 'Tour de 2 días al Nevado de Toluca todo incluido', img: '../images/nevado2.jpg'},
-    {id: 2, name: 'TourB- Peñoles Chihuahua', price:80, category:  'escalada', stock:10, description: 'Tour de 2 días a Peñoles Chihuahua', img: '../images/peñoles.jpeg'},
-    {id: 3, name: 'TourC Sierra Gorda S.L.P', price:120, category: 'senderismo', stock:15, description: 'Tour de 2 días a Sierra Gorda en S.L.P todo incluido', img: '../images/sierraGorda.jpg'}
+    {id: 1, name: 'TourA- Nevado de Toluca', price:100, category: 'biking', stock:25, description: 'Tour de 2 días al Nevado de Toluca todo incluido', img: '../images/nevado2.jpg', serviciosBrindados: 'traslado redondo, alimentos, bicicleta y equipo de seguridad requerido por persona', puntoPartida: 'Zócalo CDMX', reserva: 'Máximo 3 días previos a la fecha de salida', fechas: 'primer y tercer viernes de cada mes'},
+    {id: 2, name: 'TourB- Peñoles Chihuahua', price:80, category:  'escalada', stock:10, description: 'Tour de 2 días a Peñoles Chihuahua', img: '../images/peñoles.jpeg', serviciosBrindados: 'equipo de escalada, traslado al sitio y zona de escalada, alimentos, equipo de camping', puntoPartida: 'Centro Histórico de Chihuahua', reserva: '1 semana de antelación', fechas: 'último fin de semana de cada mes'},
+    {id: 3, name: 'TourC Sierra Gorda S.L.P', price:120, category: 'senderismo', stock:15, description: 'Tour de 2 días a Sierra Gorda en S.L.P todo incluido', img: '../images/sierraGorda.jpg', serviciosBrindados: 'traslado redondo a la zona de senderismo, equipo de camping, guía de sendero, bastones, aguay alimentos por persona', puntoPartida: 'centro Histórico de Querétaro', reserva: '3 días de antelación', fechas: 'primer sábado de cada mes'}
 ]
 
 export const getProducts = () => {
