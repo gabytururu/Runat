84ad4cd8e81bad2f17b3051a75511d67d6f2d4d3
gabriela tururu <gabriela.aguayaire@gmail.com>
2022-04-22 08:03:42 -0500

    Clase10-tecnicasDeRendering_puente_D10CartView

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
             <Routes>
               <Route path="/" element={<ItemListContainer/>} />
               <Route path="/category/:categoryId" element={<ItemListContainer/>} />
-              <Route path="/detail/:productId" element={<ItemDetailContainer />} />              
+              <Route path="/detail/:productId" element={<ItemDetailContainer />} />                            
               <Route path="*" element={<h1>404 NOT FOUND</h1>} />   
             </Routes>
           </BrowserRouter> 
