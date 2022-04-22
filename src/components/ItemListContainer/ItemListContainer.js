389dd96e94f7fd5c748699f923df3c53de52b44e
gabriela tururu <gabriela.aguayaire@gmail.com>
2022-04-22 07:57:50 -0500

    Clase10-TecnicasDeRendering_SegundaParte-Re-render-c-Use-Memo

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -6,7 +6,8 @@ import {useParams} from 'react-router-dom'
 const ItemListContainer = (props) => {
 
     const [products, setProducts] = useState([])
-
+    //creando estado del titulo
+    const [ title, setTitle ] = useState()
     const { categoryId } = useParams()
     
 
@@ -14,12 +15,22 @@ const ItemListContainer = (props) => {
         getProducts(categoryId).then(prods => {
             setProducts(prods)}
             )
-    }, [categoryId])    
+    }, [categoryId])  
+    
+    // creando el efecto para que aparezca el titulo a los 2 segundos para ejemplificar el re-rendering y demostrar la importancia del memo para evitar re-render de todos los hijos (ej titulo + ItemList), y en cambio, simplemente re-renderizar lo que debe renderizarse (que en este caso es title) -- esta es una técnica de optimización importante
+    // ir a ItemList para ver el uso de memo para evitar ese re rendering
+    // nota importante, la importación y consumo del memo tiene que utilizarse en el componente que NO DEBERÍA CAMBIAR por un CAMBIO DE ESTADO --- EN este caso es el componente de ItemList
+    useEffect (() => {
+        setTimeout(() => {
+            setTitle('Este título se agrega a los 2 segundos')
+        }, 2000)
+    })
 
     return (
 
         <div>
-            <h1>{props.greeting}</h1>                       
+            <h1>{props.greeting}</h1> 
+            <h1>{title}</h1>                  
             <ItemList products={products}/>
         </div>
 
